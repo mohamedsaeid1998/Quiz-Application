7bd05a4ac9ee542abfcb8b9eefddbe57d41326f2
abdelrhmaneladawy32 <abdelrhmaneladawy32@gmail.com>
2024-02-07 10:11:58 +0200

    create Home Ui and setting language

--- src/App.tsx
+++ src/App.tsx
@@ -3,38 +3,50 @@ import { Provider } from "react-redux";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import Store from "./Redux/Store";
-import { AuthLayout, MasterLayout,ProtectedRoute } from "./Components";
-import { ForgetPassword, Login, NotFound, Register, ResetPassword } from "./Pages";
+import { AuthLayout, MasterLayout, ProtectedRoute } from "./Components";
+import {
+  ForgetPassword,
+  Groups,
+  Home,
+  Login,
+  NotFound,
+  Quizzes,
+  Register,
+  ResetPassword,
+  Results,
+} from "./Pages";
 import ChangePassword from "./Pages/Auth/ChangePassword/ChangePassword";
+import Help from "./Pages/Help/Help";
 // import LoadingComponent from "./Components/Loading/Loading";
 
 function App() {
-
-const routes = createBrowserRouter([
-
-
-
-  {
-    path: "/",
-    element: <AuthLayout />,
-    errorElement: <NotFound />,
-    children: [
-      { index: true, element: <Login /> },
-      { path: "register", element: <Register /> },
-      { path: "forget-password", element: <ForgetPassword /> },
-      { path: "reset-password", element: <ResetPassword /> },
-      { path: "change-password", element: <ChangePassword/> },
-    ],
-  },
-
+  const routes = createBrowserRouter([
     {
-      path: "dashboard",element:<MasterLayout />,errorElement: <NotFound />,children: [
-        // {index: true, element:<Home/>},
-        // { path: "users", element: <Users /> },
+      path: "/",
+      element: <AuthLayout />,
+      errorElement: <NotFound />,
+      children: [
+        { index: true, element: <Login /> },
+        { path: "register", element: <Register /> },
+        { path: "forget-password", element: <ForgetPassword /> },
+        { path: "reset-password", element: <ResetPassword /> },
+        { path: "change-password", element: <ChangePassword /> },
       ],
     },
 
-
+    {
+      path: "dashboard",
+      element: <MasterLayout />,
+      errorElement: <NotFound />,
+      children: [
+        { index: true, element: <Home /> },
+        { path: "home", element: <Home /> },
+        { path: "groups", element: <Groups /> },
+        { path: "quizzes", element: <Quizzes /> },
+        { path: "results", element: <Results /> },
+        { path: "help", element: <Help /> },
+      ],
+    },
   ]);
 
   return (
@@ -43,7 +55,7 @@ const routes = createBrowserRouter([
         <RouterProvider router={routes} />
       </Provider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './Styles/global.scss'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./i18n";
+import App from "./App.tsx";
+import "./Styles/global.scss";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
